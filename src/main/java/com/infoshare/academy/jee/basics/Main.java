07c649c28b6419c91e5c14a88feabedfa8bd64a4
Anna Skulimowska <annamskulimowska@gmail.com>
2019-01-23 19:17:32 +0100

    Add maven project

---
+++ src/main/java/com/infoshare/academy/jee/basics/Main.java
@@ -0,0 +1,8 @@
+package com.infoshare.academy.jee.basics;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello!");
+    }
+}
---
+++ src/main/java/com/infoshare/academy/jee/basics/UsersRepository.java
@@ -0,0 +1,34 @@
+package com.infoshare.academy.jee.basics;
+
+//
+//public class UsersRepository {
+//    private static List<User> usersRepository = new ArrayList<>();
+//
+//    public static List<User> getUsers() {
+//        if (usersRepository.size() == 0) {
+//            fillRepositoryWithDefaults();
+//        }
+//        return usersRepository;
+//    }
+//
+//    private static void fillRepositoryWithDefaults() {
+//        User user1 = new User(1, "Jan", "Muzykant", "secret", 21);
+//        usersRepository.add(user1);
+//
+//        User user2 = new User(2, "Adam", "Ad", "admin",35);
+//        usersRepository.add(user2);
+//
+//        User user3 = new User(3, "Zuzanna", "Zuz", "1234", 32);
+//        usersRepository.add(user3);
+//    }
+//
+//    public static boolean contains(User user) {
+//        List<User> repository = getUsers();
+//        for (User userFromList : repository) {
+//            if (userFromList.getId() == user.getId()) {
+//                return true;
+//            }
+//        }
+//        return false;
+//    }
+//}
