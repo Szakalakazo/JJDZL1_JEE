97ca1228ca00b40e2a6df21663f9869288cc7123
Szakalakazo <tomek.starzak@gmail.com>
2019-01-27 16:04:05 +0100

    Add exercise 13 - search by id

--- src/main/java/com/infoshare/academy/jee/basics/servlets/FindUSerByIdServlet.java
+++ src/main/java/com/infoshare/academy/jee/basics/servlets/FindUSerByIdServlet.java
@@ -1,4 +1,50 @@
 package com.infoshare.academy.jee.basics.servlets;
 
-public class FindUSerByIdServlet {
+import com.infoshare.academy.jee.basics.dao.UsersRepositoryDao;
+import com.infoshare.academy.jee.basics.dao.UsersRepositoryDaoBean;
+import com.infoshare.academy.jee.basics.domain.User;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.List;
+
+@WebServlet("/find-user-by-id")
+public class FindUSerByIdServlet extends HttpServlet {
+
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        String id = request.getParameter("id");
+
+        UsersRepositoryDao dao = new UsersRepositoryDaoBean();
+
+        PrintWriter printWriter = response.getWriter();
+
+
+        if (request.getParameter("id") == null || request.getParameter("id").isEmpty()) {
+            response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+        } else {
+
+            try {
+                User userById = dao.getUserById(Integer.parseInt(id));
+                printWriter.write("User ID: " + userById.getId());
+                printWriter.write("User name: " + userById.getName());
+                printWriter.write("User login: " + userById.getLogin());
+                printWriter.write("User age: " + userById.getAge());
+
+
+            } catch (IllegalArgumentException exc) {
+                printWriter.write("User with this Id does not exist!");
+            }
+
+
+        }
+
+    }
 }
